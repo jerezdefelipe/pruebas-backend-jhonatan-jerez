624d010e99b73ce461d1e3208c40bb8921561b10
jerezdefelipe <jerezdefelipe@gmail.com>
2025-07-06 21:14:41 -0500

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import booksRoutes from './src/routes/books.routes.js';
-
-const app = express();
-app.use(bodyParser.json());
-app.use('/libros', booksRoutes);
-
-export default app;
